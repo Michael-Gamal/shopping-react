988e09420d2e5cedfe3e27c738612ad7b5fab555
Michael-Gamal <michealgamal242@gmail.com>
2025-08-21 22:21:15 +0300

    General repairs

--- src/component/About/AboutusFueaures.jsx
+++ src/component/About/AboutusFueaures.jsx
@@ -4,42 +4,42 @@ import { FaUsersLine, FaQuoteLeft } from 'react-icons/fa6'
 const AboutusFueaures = () => {
   return (
     <div>
+        <div className="box">
+                <div className="icon">
+                    <RiSecurePaymentFill />
+                </div>
+                <div className="text">
+                    <h5>Fast & Secure</h5>
+                    <p>Optimized preformance</p>
+                </div>
+            </div>
             <div className="box">
-                    <div className="icon">
-                        <RiSecurePaymentFill />
-                    </div>
-                    <div className="text">
-                        <h5>Fast & Secure</h5>
-                        <p>Optimized preformance</p>
-                    </div>
+                <div className="icon">
+                    <RiSoundModuleLine />
                 </div>
-                <div className="box">
-                    <div className="icon">
-                        <RiSoundModuleLine />
-                    </div>
-                    <div className="text">
-                        <h3>Advanced Filtering</h3>
-                        <p>Optimized preformance</p>
-                    </div>
+                <div className="text">
+                    <h3>Advanced Filtering</h3>
+                    <p>Optimized preformance</p>
                 </div>
-                <div className="box">
-                    <div className="icon">
-                        <FaUsersLine />
-                    </div>
-                    <div className="text">
-                <h5 >User Reviews</h5>
-                <p>Ratings & feedback</p>
-                    </div>
+            </div>
+            <div className="box">
+                <div className="icon">
+                    <FaUsersLine />
                 </div>
-                <div className="box">
-                    <div className="icon">
-                        <TbLocation />
-                    </div>
-                    <div className="text">
-                        <h3>Order Tracking</h3>
-                        <p>Live order status</p>
-                    </div>
+                <div className="text">
+            <h5 >User Reviews</h5>
+            <p>Ratings & feedback</p>
                 </div>
+            </div>
+            <div className="box">
+                <div className="icon">
+                    <TbLocation />
+                </div>
+                <div className="text">
+                    <h3>Order Tracking</h3>
+                    <p>Live order status</p>
+                </div>
+        </div>
     </div>
   )
 }
